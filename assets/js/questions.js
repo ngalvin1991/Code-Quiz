82a1b0018f70342ede58f3273a8ad7f61302716b
ngalvin1991 <117473760+ngalvin1991@users.noreply.github.com>
2023-01-13 19:23:13 +0000

    Added all questions for the assignment in this file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,11 +1,73 @@
 
-// var quizQuestions =[
+var quizQuestions =[
+    {
+        question: "Which of the following keywords is used to define a variable in Javascrip?",
+        choices: ["var", "let", "Both A & B", "None of the above"],
+        correctAnswerIndex: "Both A & B"
+    
+    },
+
+    {
+        question: "Which of the following methods is used to access HTML elements using Javascript? ",
+        choices: ["getElementbyId()", "getElementbyClassName()", "Both A & B", "None of the above"],
+        correctAnswerIndex: "Both A & B"
+    
+    },
+
+    {
+        question: "Which of the following methods can be used to display data in some form using Javascript?",
+        choices: ["document.write()", "console.log", "window.alert()", "All of the above"],
+        correctAnswerIndex: "All of the above"
+    
+    },
+
+    {
+        question: "How can a datatype be declared to be a constant type?",
+        choices: ["const", "var", "let", "constant"],
+        correctAnswerIndex: "const"
+    
+    },
+
+    {
+        question: "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+        choices: ["Boolean", "Undefined", "Object", "Integer"],
+        correctAnswerIndex: "Object"
+    
+    },
+
+    {
+        question: "What does the Javascript “debugger” statement do?",
+        choices: ["It will debug all the errors in the program at runtime", "It acts as a breakpoint in a program", "It will debug error in the current statement if any", 
+        "All of the above"],
+        correctAnswerIndex: "It acts as a breakpoint in a program"
+    
+    },
+
+    {
+        question: "Which of the following is not a Javascript framework?",
+        choices: ["Node", "Vue", "React", "Cassandra"],
+        correctAnswerIndex: "Cassandra"
+    
+    },
+
+    {
+        question: "How do we write a comment in javascript?",
+        choices: ["/**/", "//", "#", "$ $"],
+        correctAnswerIndex: "//"
+    
+    },
+
+    {
+        question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+        choices: ["Throws an error", "Ignores the statements", "Gives a warning", "None of the above"],
+        correctAnswerIndex: "Ignores the statements"
+    
+    }
+];
 
 
-// ]
 
 
-// correctAnswerIndex: 
 
 
 
