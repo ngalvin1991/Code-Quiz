9c0e8aae62b453a2ac62dcc483887b08c14c250b
ngalvin1991 <117473760+ngalvin1991@users.noreply.github.com>
2023-01-16 19:23:18 +0000

    added 4 functions including startQuiz, giveQuestion, clickQuestion and clockCountDown

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -16,21 +16,103 @@ var answerIsCorrect = 0;
 var questionNumber = 0;
 var totalScore = 0;
 var questionIndex = 0;
+var gameOver 
+var initialNumMax = 3;
+var timeId;
 
-//getElementsbyID variables
-var timer = document.getElementById("timer");
-var startDiv = document.getElementById("start");
-var questionsDiv = document.getElementById("questions")
-var choicesDiv = document.getElementById("choices")
+var questionsEl = document.querySelector("#questions");
+var timerEl = document.querySelector("#timer");
+var submitButton = document.querySelector("#submit");
+var choicesEl = document.querySelector("#choices");
+var startButton = document.querySelector("#start")
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
+var clearHs = document.querySelector("#clear");
+
+
+var startQuiz = function () {
+    timeId = setInterval(1000);
+    timerEl.textContent = 180;
+    var mainPage = document.getElementById("start-screen");
+    mainPage.setAttribute("class", "hide");
+    questionsEl.removeAttribute("class");
+    giveQuestion ()
+}
+
+function giveQuestion () {
+    var giveQuestion = quizQuestions[questionIndex];
+    var questionEl = document.getElementById("question-title")
+    questionEl.textContent = giveQuestion.question;
+    choicesEl.innerHTML = "";
+    giveQuestion.choices.forEach(function(choice,i){
+        var choicesBtn = document.createElement("button");
+        choicesBtn.setAttribute("value", choice);
+        choicesBtn.textContent = i + 1 + ". " + choices;
+        choicesBtn.onclick = clickQuestion;
+        choicesEl.appendChild(choicesBtn);
+
+    });
+}
+
+function clickQuestion () {
+    if (this.value !== question[questionIndex].correctAnswerIndex) {
+        time -= 10;
+        if (time < 0) {
+            time = 0;
+        }
+        timerEl.textContent = time;
+        feedbackEl.textContent = "Incorrect!!! :("
+    } else {
+        feedbackEl.textContent = "Correct! Well Done! :)"
+    }
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+        feedbackEl.setAttribute("class", "feedback hide");  
+    }, 1000);
+
+    questionIndex++; 
+    if (questionIndex === question.length) {
+        endQuiz ();
+    } else {
+        giveQuestion ();
+    }
+}
+
+function clockCountDown () {
+    time--; 
+    timerEl.textContent = time;
+    if (time <= 0) {
+        endQuiz();
+    }
+}
 
 
 
-//The timer will begin when the start button is pressed. 
+// //The timer will begin when the start button is pressed. 
 // var timeLength = 180;
 // function startQuiz() {
 //     questionIndex = 0;
 //     timeLength = 180;
 //     timer.textContent = timeLength;
 
-    
+//     var startTimer = setInterval(function() {
+//         timeLength --;
+//         timer.textContent = timeLength;
+//         if (timeLength <= 0) {
+//             clearInterval(startTimer)
+//             if (questionIndex < quizQuestions - 1) {
+//                 gameOver ();
+//             }
+//         }
+//     }, 1000);
+
+//     seeQuiz() 
+// };
+
+// function seeQuiz() {
+//     anotherQuestion ();
 // }
+
+// function nextQuestion () {
+// questionTitle.textContent = quizQuestions[questionIndex].question;
+// }   
